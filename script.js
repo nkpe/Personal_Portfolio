8dfed0965d142e676593c4cd65cdb09acf2aa9c7
Naomi <63055040+nkpe@users.noreply.github.com>
2022-05-12 11:11:33 +0100

    Links working
    Links working in footer (github, LinkdIn and Contact Me)
    
    Added Copyright to footer & icon to window tab
    
    Put all these changes to each page

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const aboutP = document.getElementById("p_about");
 const aboutMeBlock = document.getElementById("about_me")
 const bkgdH2 = document.getElementById("h2_bkgd");
 const bkgdP = document.getElementById("p_bkgd");
+const bioBkgBlock = document.getElementById("bio-background");
 const presentH2 = document.getElementById("h2_present");
 const presentP = document.getElementById("p_present");
+const presentBlock = document.getElementById("bio-present");
 const interestsH2 = document.getElementById("h2_interests");
 const interestsP = document.getElementById("p_interests");
+const interestBlock = document.getElementById("bio-interests"); 
 
 
 
-function aboutOpenClose(){
+const aboutOpenClose = () => {
     console.log(document.documentElement.scrollTop);
      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         aboutH2.style.display = "none";
@@ -20,13 +23,14 @@ function aboutOpenClose(){
         aboutH2.style.display = "block";
         aboutP.style.display = "none";
         aboutMeBlock.style.filter = "brightness(100%)";
-    } else if (document.body.scrollTop > 300 || document.socumentElement.scrollTop > 300){
+    } else if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
         bkgdH2.style.display = "none";
         bkgdP.style.display = "block";
-    }
+        bioBkgBlock.style.display = "brightness(90%)";
+    } 
 };
 
-window.onscroll = function() {
+window.onscroll = () => {
     aboutOpenClose();
 };
 
