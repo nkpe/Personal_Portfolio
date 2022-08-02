770a0ab5011019f0d35e147284165b12a2417091
Naomi <63055040+nkpe@users.noreply.github.com>
2022-08-02 13:14:20 +0100

    portfolio layout WIP

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -8,31 +8,6 @@ const portfolioSections = document.getElementById("portf-section");
 const codeProjects = document.getElementById("portf-code");
 const designProjects = document.getElementById("portf-design");
 
-window.onload = function (e) {
-   if (pageHTML === "portfolio.html"){
-    if (e.target.hash === "#portf-code") {
-        alert("#portf-code running");
-        codeProjects.style.display = "flex";
-        designProjects.style.display = "none";
-    } else if (e.target.hash === "#portf-design") {
-        codeProjects.style.display = "none";
-        designProjects.style.display = "flex";
-    };
-}};
-
-let pageHTML = null;
-let pageShow = (e) => {
-    console.log(e);
-    portfolioDropdown.style.display = "none";
-    let pageHTMLArray = location.pathname.split("/");
-    pageHTML = pageHTMLArray[pageHTMLArray.length-1];
-    console.log(pageHTML);
-};
-
-for (let link of dropdownLinks) {
-    link.addEventListener('click', pageShow);
-};
-
 let dropdownState = null;
 
 let onLoad = () => {
