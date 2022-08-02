24cfe63cc61f6af1260bedfe3534de75fe1d1d4d
Naomi <63055040+nkpe@users.noreply.github.com>
2022-08-02 10:13:00 +0100

    dropdown links in progress

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -8,13 +8,25 @@ const portfolioSections = document.getElementById("portf-section");
 const codeProjects = document.getElementById("portf-code");
 const designProjects = document.getElementById("portf-design");
 
+window.onload = function (e) {
+   if (pageHTML === "portfolio.html"){
+    if (e.target.hash === "#portf-code") {
+        alert("#portf-code running");
+        codeProjects.style.display = "flex";
+        designProjects.style.display = "none";
+    } else if (e.target.hash === "#portf-design") {
+        codeProjects.style.display = "none";
+        designProjects.style.display = "flex";
+    };
+}};
+
+let pageHTML = null;
 let pageShow = (e) => {
     console.log(e);
-    if (e.target.hash === "#portf-code") {
-        codeProjects.style.display = "block";
-    } else if (e.target.hash === "#portf-design") {
-        designProjects.style.display = "block";
-    }
+    portfolioDropdown.style.display = "none";
+    let pageHTMLArray = location.pathname.split("/");
+    pageHTML = pageHTMLArray[pageHTMLArray.length-1];
+    console.log(pageHTML);
 };
 
 for (let link of dropdownLinks) {
@@ -33,7 +45,6 @@ let navDropdownShow = (e) => {
     if (dropdownState === false){
     console.log(dropdownState);
     portfolioDropdown.style.display = "flex";
-    console.log(e);
     dropdownState = true;
     console.log(dropdownState);
 } else {
